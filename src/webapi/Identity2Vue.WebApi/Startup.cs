7b9a214a6f27bb9da05f5026aa19d7fffc0191f8
allen lian <allen.lian@outlook.com>
2021-08-30 14:46:49 +0800

    add cors setting.

--- src/webapi/Identity2Vue.WebApi/Startup.cs
+++ src/webapi/Identity2Vue.WebApi/Startup.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Linq;
 using Identity2Vue.WebApi.Services;
-using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -12,87 +11,94 @@ using Microsoft.OpenApi.Models;
 
 namespace Identity2Vue.WebApi
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-
-      services.AddControllers();
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Identity2Vue.WebApi", Version = "v1" });
-      });
-
-      services.AddAuthentication("Bearer")
-          .AddJwtBearer("Bearer", options =>
-          {
-            options.Authority = Configuration["JwtToken:Authority"];
-            // options.AutomaticRefreshInterval = TimeSpan.FromSeconds(10);
-            options.RequireHttpsMetadata = true;
-            options.MapInboundClaims = false;
-            options.TokenValidationParameters = new TokenValidationParameters{
-              ValidateAudience = false,
-              ClockSkew = TimeSpan.Zero
-            };
-            options.SaveToken = true;
-          });
-
-      services.AddAuthorization(options =>
-      {
-        options.AddPolicy("platform.scopes", policy =>
+        public Startup(IConfiguration configuration)
         {
-          policy.RequireAuthenticatedUser();
-          policy.RequireAssertion(ctx =>
-          {
-            return null != ctx.User.Claims.FirstOrDefault(c => c.Type == "scope" && c.Value == "platform.api");
-          });
-        });
-      });
+            Configuration = configuration;
+        }
 
-      services.AddScoped<IProductService, ProductService>();
+        public IConfiguration Configuration { get; }
 
-      services.AddCors(options => {
-        options.AddDefaultPolicy(policy => {
-          policy.WithOrigins("http://localhost:8080")
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .Build();
-        });
-      });
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Identity2Vue.WebApi", Version = "v1" });
+            });
+
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = Configuration["JwtToken:Authority"];
+                    // options.AutomaticRefreshInterval = TimeSpan.FromSeconds(10);
+                    options.RequireHttpsMetadata = true;
+                    options.MapInboundClaims = false;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                    options.SaveToken = true;
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("platform.scopes", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireAssertion(ctx =>
+                    {
+                        return null != ctx.User.Claims.FirstOrDefault(c => c.Type == "scope" && c.Value == "platform.api");
+                    });
+                });
+            });
+
+            services.AddScoped<IProductService, ProductService>();
+
+            var origins = Configuration["Cors:Origins"]?.Split(',');
+            if (origins != null && origins.Length > 1)
+            {
+                services.AddCors(options =>
+                {
+                    options.AddDefaultPolicy(policy =>
+                    {
+                        policy.WithOrigins(origins)
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .Build();
+                    });
+                });
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Identity2Vue.WebApi v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers()
+                  //.RequireAuthorization("platform.scopes")
+                ;
+            });
+        }
     }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Identity2Vue.WebApi v1"));
-      }
-
-      app.UseHttpsRedirection();
-
-      app.UseRouting();
-      app.UseCors();
-
-      app.UseAuthentication();
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers()
-          //.RequireAuthorization("platform.scopes")
-          ;
-      });
-    }
-  }
 }
